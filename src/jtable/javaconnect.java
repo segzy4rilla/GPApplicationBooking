81b4550d15c825d57585a8417774a65434643325
Godwinstuff <Godwinstuff@192.168.122.1>
2015-02-12 13:49:48 +0000

    i changed the directory for my database connection because a new database is being used

--- src/jtable/javaconnect.java
+++ src/jtable/javaconnect.java
@@ -1,34 +1,30 @@
 package jtable;
 
-import java.awt.HeadlessException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import javax.swing.JOptionPane;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
- * @author Godwinstuff
+ * @author Zayn
  */
+import java.sql.*;
+import javax.swing.*;
 public class javaconnect {
     
-    Connection conn = null;
+   Connection conned = null;
 
     public static Connection ConnecrDb() {
         try {
             Class.forName("org.sqlite.JDBC");
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/Godwinstuff/Documents/jtable/Medical Bookng.sqlite");
-           // JOptionPane.showMessageDialog(null, "Connected to Database");
-            return conn;
-        } catch (ClassNotFoundException | SQLException | HeadlessException e) {
+            Connection conned = DriverManager.getConnection("jdbc:sqlite:/Users/Godwinstuff/Downloads/medicalDBe.sqlite");
+            return conned;
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e);
             return null;
         }
+ 
+    
 }
 }
