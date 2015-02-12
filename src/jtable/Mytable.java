b5a7f01ccf2fe4ebd79f66cbab36ad0b215c54ba
Godwinstuff <Godwinstuff@eroam-160-57.wifi.wmin.ac.uk>
2015-02-12 16:28:10 +0000

    fixed the doctor name changing bug and tested code to work

--- src/jtable/Mytable.java
+++ src/jtable/Mytable.java
@@ -83,7 +83,7 @@ public class Mytable {
     public void makeframe() {
      Collections.sort(Appointmentdetails);
         for (int i = 0; i < Appointmentdetails.size(); i++) {
-           System.out.println(Appointmentdetails.get(i).getTime());
+           System.out.println(Appointmentdetails.get(i).getPName()+" " +Appointmentdetails.get(i).getGPName()+" "+Appointmentdetails.get(i).getTime());
         }
         String[] Columnnames = {"Doctors Name", "9:30am-10:00am", "10:00am-10:30am", "10:30am-11:00am", "13:00pm-13:30pm", "13:30pm-14:00pm", "14:00pm-14:30pm"};
         Apptinfo = new JLabel("Appointment Info:");
@@ -167,7 +167,9 @@ public class Mytable {
                 int row, column;
                 row = table.rowAtPoint(point);
                 column = table.columnAtPoint(point);
-              if(!table.getValueAt(row, column).equals("BOOKED")){    
+           
+                if (column!=0){
+                if(!table.getValueAt(row, column).equals("BOOKED")){    
                 if (currentrow==0&& currentcolumn == 0){
                   
                     currentrow = row;
@@ -183,6 +185,8 @@ public class Mytable {
                  table.repaint();
                 }
               }
+            }
+                
                 if (table.getValueAt(row, column).equals("FREE") || table.getValueAt(row, column).equals("BOOKED")) {
                     if (table.getValueAt(row, column).equals("BOOKED")) {
 
